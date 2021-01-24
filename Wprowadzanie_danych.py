78f1ef366a73f187a33ad900b4667fbeb7ac66aa
FilipImiela <77736946+FilipImiela@users.noreply.github.com>
2021-01-24 23:58:53 +0100

    Add files via upload

---
+++ Wprowadzanie_danych.py
@@ -0,0 +1,10 @@
+
+def main():
+    F_name, L_name, y_of_birth = input("Enter first name, last name and year of birth: ").split()
+    print(F_name)
+    print(L_name)
+    print(y_of_birth)
+
+
+if __name__ == "__main__":
+    main()
